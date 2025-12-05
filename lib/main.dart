79c3573af670e5d74996bba5bc315274752f7bda
Sony Subagyo <sonysubagyo0852@gmail.com>
2025-12-05 22:42:29 +0700

    menghapus appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:curesee/features/beranda/presentation/pages/beranda_page.dart';
-import 'package:curesee/features/history/presentation/pages/history_page.dart';
-import 'package:curesee/features/more/presentation/pages/more_page.dart';
 import 'package:curesee/features/profile/presentation/pages/profil_page.dart';
 import 'package:curesee/features/camera/presentation/pages/camera_page.dart';
+import 'package:curesee/features/history/presentation/pages/history_page.dart';
+import 'package:curesee/features/more/presentation/pages/more_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -43,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("CureSee")),
+      // appBar: AppBar(title: const Text("CureSee")),
       
       // halaman akan berubah berdasarkan _currentIndex
       body: IndexedStack(
