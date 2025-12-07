87efcc7381bf35d8945890f722dc1a816280f929
Refida-bit <refidaseptianaputri0@gmail.com>
2025-12-07 21:48:28 +0700

    ngisi data: souce, repo

---
+++ lib/features/profile/data/data_souce/profile_remote_data_source.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:curesee/features/profile/domain/entities/profil.dart';
+import 'package:http/http.dart' as http;
+
+final String baseUrl = "";
+
+class ProfileRemoteDataSource {
+  Future<List<Profile>> getAllProfile() async {
+    final response = await http.get(Uri.parse(baseUrl));
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data
+          .map(
+            (item) => Profile(
+              userid: int.parse(item['userid']),
+              name: item['name'],
+              gender: item['gender'],
+              email: item['email'],
+              ege: int.parse(item['ege']),
+            ),
+          )
+          .toList();
+    } else {
+      throw Exception("gagal memuat profile");
+    }
+  }
+
+  Future<void> updateProfile(int userid, Profile fp) async {
+    final response = await http.put(
+      Uri.parse("$baseUrl/$userid"),
+      headers: {"content-Type": "application/json"},
+      body: jsonEncode({
+        "userid": fp.userid,
+        "name": fp.name,
+        "gender": fp.gender,
+        "email": fp.email,
+        "ege": fp.ege,
+      }),
+    );
+    if (response.statusCode != 200) {
+      throw Exception("gagal update profile");
+    }
+  }
+}
