3d5e8ac50dd462c5c2780e7613c643084ac41363
Refida-bit <refidaseptianaputri0@gmail.com>
2025-12-07 20:28:26 +0700

    memperbaiki domain profile (pada profile pengguna tidak bisa delete profile

--- lib/features/profile/domain/repositories/profil_repository.dart
+++ lib/features/profile/domain/repositories/profil_repository.dart
@@ -3,5 +3,4 @@ import 'package:curesee/features/profile/domain/entities/profil.dart';
 abstract class ProfileRepository {
   Future<List<Profile>> getAllProfile();
   Future<void> updateProfile(int userid, Profile profile);
-  Future<void> deleteProfile(int userid);
 }
