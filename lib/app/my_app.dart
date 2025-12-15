8925a9206317bcbe1cc5361e07a77da625e56879
Sony Subagyo <sonysubagyo0852@gmail.com>
2025-12-15 18:06:40 +0700

    modifikasi

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,42 +1,42 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'login/presentation/bloc/login_bloc.dart';
-import 'login/domain/use_case/login_usecase.dart';
-import 'login/data/data_source/login_remote_datasource.dart';
-import 'login/data/repositories/login_repository_impl.dart';
-import 'login/presentation/pages/login_page.dart';
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final dataSource = DummyLoginDataSource();
-    final repo = LoginRepositoryImpl(dataSource);
-    final usecase = LoginUsecase(repo);
-
-    return BlocProvider(
-      create: (_) => LoginBloc(usecase),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: LoginPage(),
-      ),
-    );
-  }
-}
-
 // import 'package:flutter/material.dart';
-// import 'home_layout.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// import 'login/presentation/bloc/login_bloc.dart';
+// import 'login/domain/use_case/login_usecase.dart';
+// import 'login/data/data_source/login_remote_datasource.dart';
+// import 'login/data/repositories/login_repository_impl.dart';
+// import 'login/presentation/pages/login_page.dart';
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
 
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: const HomeLayout(),
+//     final dataSource = DummyLoginDataSource();
+//     final repo = LoginRepositoryImpl(dataSource);
+//     final usecase = LoginUsecase(repo);
+
+//     return BlocProvider(
+//       create: (_) => LoginBloc(usecase),
+//       child: const MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: LoginPage(),
+//       ),
 //     );
 //   }
 // }
+
+import 'package:flutter/material.dart';
+import 'home_layout.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeLayout(),
+    );
+  }
+}
