ede7c7b3143309f7c414e2f28c4004458fe6e239
Sony Subagyo <sonysubagyo0852@gmail.com>
2025-12-16 21:14:19 +0700

    memperbaiki my_app

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:curesee/app/home_layout.dart';
 import 'package:curesee/app/navigation/app_pages.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       create: (_) => LoginBloc(usecase),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomeLayout(),
+        home: LoginPage(),
       ),
     );
   }
