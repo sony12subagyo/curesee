6d1d96689f64c3456ae28dd746e1d555b8c76d7e
Sony Subagyo <sonysubagyo0852@gmail.com>
2025-12-29 21:50:44 +0700

    merubah ngrok

--- lib/app/login/data/repositories/login_repository_impl.dart
+++ lib/app/login/data/repositories/login_repository_impl.dart
@@ -17,7 +17,7 @@ class LoginRepositoryImpl implements LoginRepository {
   @override
   Future<String> fetchRole(String token) async {
     final response = await http.get(
-      Uri.parse('https://d99c7493d460.ngrok-free.app/api/profile'),
+      Uri.parse('https://a9faa2a6661c.ngrok-free.app/api/profile'),
       headers: {
         'Authorization': 'Bearer $token',
         'Accept': 'application/json',
