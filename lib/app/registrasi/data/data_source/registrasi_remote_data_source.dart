41377749341fa4a85e871a3d1c045552abe66c0e
Firman270 <firmn030@gmail.com>
2026-01-10 02:11:13 +0700

    memperbaiki conflict

--- lib/app/registrasi/data/data_source/registrasi_remote_data_source.dart
+++ lib/app/registrasi/data/data_source/registrasi_remote_data_source.dart
@@ -5,7 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class RegistrasiRemoteDataSource {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  final String baseUrl = 'https://9d1c81950154.ngrok-free.app/api/register';
+  final String baseUrl = 'https://dbd21fec81a1.ngrok-free.app/api/register';
 
   Future<void> register(RegistrasiEntity entity) async {
     try {
--- lib/users/features/history/presentation/pages/history_page.dart
+++ lib/users/features/history/presentation/pages/history_page.dart
@@ -1,76 +1,45 @@
-// // import 'package:flutter/material.dart';
-// // import '../widgets/history_app_bar.dart';
-// // import '../widgets/history_card.dart';
 
-// // class HistoryPage extends StatelessWidget {
-// //   const HistoryPage({super.key});
+import 'dart:io';
+import 'package:curesee/users/features/history/presentation/bloc/history_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Column(
-// //       children: [
-// //         // const HistoryAppBar(),
+class HistoryPage extends StatelessWidget {
+  const HistoryPage({super.key});
 
-// //         Expanded(
-// //           child: ListView.builder(
-// //             padding: const EdgeInsets.symmetric(horizontal: 16),
-// //             itemCount: 6,
-// //             itemBuilder: (context, index) {
-// //               return HistoryCard(
-// //                 onTap: () {
-// //                   // nanti arahkan ke detail via router / cubit
-// //                 },
-// //               );
-// //             },
-// //           ),
-// //         ),
-// //       ],
-// //     );
-// //   }
-// // }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: const Text("Riwayat Scan",style: TextStyle(color: Colors.blue),))),
+      body: BlocBuilder<HistoryBloc, HistoryState>(
+        builder: (context, state) {
+          if (state is HistoryLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
+          if (state is HistoryLoadedState) {
+            // if (state.scans.isEmpty) {
+            //   return const Center(child: Text("Belum ada hasil scan"));
+            // }
 
-// import 'dart:io';
-// import 'package:curesee/users/features/history/presentation/bloc/history_bloc.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
+            // return ListView.builder(
+            //   itemCount: state.scans.length,
+            //   itemBuilder: (context, i) {
+            //     final scan = state.scans[i];
+            //     return ListTile(
+            //       leading: Image.file(File(scan.imagePath), width: 56),
+            //       title: Text(scan.description.toUpperCase()),
+            //       subtitle: Text(
+            //         "${(scan.confidence * 100).toStringAsFixed(1)}% • ${scan.createdAt}",
+            //       ),
+            //     );
+            //   },
+            // );
+          }
 
-// class HistoryPage extends StatelessWidget {
-//   const HistoryPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Center(child: const Text("Riwayat Scan",style: TextStyle(color: Colors.blue),))),
-//       body: BlocBuilder<HistoryBloc, HistoryState>(
-//         builder: (context, state) {
-//           if (state is HistoryLoading) {
-//             return const Center(child: CircularProgressIndicator());
-//           }
-
-//           if (state is HistoryLoaded) {
-//             if (state.scans.isEmpty) {
-//               return const Center(child: Text("Belum ada hasil scan"));
-//             }
-
-//             return ListView.builder(
-//               itemCount: state.scans.length,
-//               itemBuilder: (context, i) {
-//                 final scan = state.scans[i];
-//                 return ListTile(
-//                   leading: Image.file(File(scan.imagePath), width: 56),
-//                   title: Text(scan.label.toUpperCase()),
-//                   subtitle: Text(
-//                     "${(scan.confidence * 100).toStringAsFixed(1)}% • ${scan.createdAt}",
-//                   ),
-//                 );
-//               },
-//             );
-//           }
-
-//           return const SizedBox();
-//         },
-//       ),
-//     );
-//   }
-// }
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+}
