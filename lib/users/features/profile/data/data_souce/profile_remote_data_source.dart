09a5805d9beb6e37cd8188a974060d8f81ccae71
Refida-bit <refidaseptianaputri0@gmail.com>
2026-01-11 23:17:31 +0700

    memasukkan ngrok untuk profile dan register yang aktif

--- lib/users/features/profile/data/data_souce/profile_remote_data_source.dart
+++ lib/users/features/profile/data/data_souce/profile_remote_data_source.dart
@@ -3,11 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:curesee/users/features/profile/domain/entities/profile.dart';
 import 'package:http/http.dart' as http;
 
-<<<<<<< HEAD
-final String baseUrl = "https://7f45403ab963.ngrok-free.app/api/profile";
-=======
+
 final String baseUrl = "https://9ded6d580cdd.ngrok-free.app/api/profile";
->>>>>>> 1a23043 (finishing profile)
+
 
 class ProfileRemoteDataSource {
   Future<Profile> getProfile() async {
