b93a64e5dd64ef2b8fd32587fb4ce6ef3c7edb85
Firman270 <firmn030@gmail.com>
2026-01-12 00:25:00 +0700

    menambahkan animasi design dan refresh indicator

--- lib/admin/features/blog/presentation/widget/blog edit/button_edit.dart
+++ lib/admin/features/blog/presentation/widget/blog edit/button_edit.dart
@@ -2,6 +2,7 @@ import 'package:curesee/admin/features/blog/core/network/admin_dio.dart';
 import 'package:curesee/admin/features/blog/data/datasource/blog_remote_datasource.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class ButtonEdit extends StatelessWidget {
   final int id;
@@ -18,7 +19,6 @@ class ButtonEdit extends StatelessWidget {
   });
 
   @override
-
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
@@ -34,6 +34,7 @@ class ButtonEdit extends StatelessWidget {
       ],
     );
   }
+
   Widget _button(String text, Color color, VoidCallback onTap) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
@@ -44,6 +45,48 @@ class ButtonEdit extends StatelessWidget {
       child: Text(text),
     );
   }
+
+  // ================= POPUP LOTTIE ANIMASI ==================
+  Future<void> _showSuccessPopup(BuildContext context, String message) async {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          width: 180,
+          height: 180,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Lottie.network(
+                "https://lottie.host/20ee79ea-4909-49fd-8bc4-195f497a8a42/XGskeXCCty.json",
+                width: 120,
+                height: 120,
+                repeat: false,
+              ),
+              Text(
+                message,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    Navigator.pop(context); // tutup popup
+  }
+
   // ================= DELETE ==================
   Future<void> _delete(BuildContext context) async {
     final dio = await AdminDio.getInstance();
@@ -51,12 +94,12 @@ class ButtonEdit extends StatelessWidget {
 
     await ds.deleteBlog(id);
 
-    ScaffoldMessenger.of(context)
-        .showSnackBar(const SnackBar(content: Text("Berhasil dihapus")));
+    await _showSuccessPopup(context, "Berhasil dihapus");
 
-    Navigator.pop(context);
+    Navigator.pop(context); // balik halaman
   }
 
+  // ================= SAVE ==================
   Future<void> _save(BuildContext context) async {
     final dio = await AdminDio.getInstance();
     final ds = BlogRemoteDatasource(dio);
@@ -65,16 +108,15 @@ class ButtonEdit extends StatelessWidget {
       '_method': 'PATCH',
       'title': titleController.text,
       'content': descController.text,
+      
       if (imagePath != null)
         'image': await MultipartFile.fromFile(imagePath!),
     });
 
     await ds.updateBlog(id, data);
 
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(const SnackBar(content: Text("Perubahan disimpan")));
+    await _showSuccessPopup(context, "Berhasil disimpan");
 
-    Navigator.pop(context);
+    Navigator.pop(context); // balik halaman
   }
 }
